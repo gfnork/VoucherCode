26e31233b03a7dbbafb149263f8ca5d22bb4e3ee
Qiong Wu <qiong.wu@gfnork.de>
2017-02-23 00:30:02 +0100

    Update app.js
    added port for azure

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function generateCode()
 }
 
 //start server
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_port = process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 app.listen(server_port, server_ip_address, function () {
     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
